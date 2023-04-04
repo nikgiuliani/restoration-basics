8e8d375b596537711874139a16d2362463524ada
nikgiuliani <anthony.giuliani26@gmail.com>
2023-04-04 17:32:25 -0600

    try again

---
+++ src/app/constants/doctrine-data.ts
@@ -0,0 +1,59 @@
+export const CORE_DOCTRINES = {
+  bookOfMormon: {
+    title: 'Book of Mormon',
+    img: '',
+    text: ['this works'],
+  },
+  priesthood: {
+    title: 'Priesthood',
+    img: '',
+    text: [
+      `
+        Anciently, Jesus Christ gave His Apostles authority to \"bind on earth\" and \"in heaven\" (Matthew 16:19) and ordained them to this power (John 15:16). 
+        With this power and authority, or priesthood, they were able to spread the gospel, establish the church, and administer ordinances after the Savior ascended into heaven (Acts).
+        Sadly, the ministries of the Apostles didn't last long. One by one, almost all were martyred, and the keys which were given to them to lead the disciples and the church
+        disappeared in a widespread, global apostasy. However, God did not intend to leave the world in darkness; centuries later, Joseph Smith had questions about which church 
+        was true. After praying in a grove of trees, he received a divine revelation that the fulness of the gospel of Jesus Christ would later be revealed to him. One of the 
+        things restored to and from Joseph Smith was the priesthood.
+      `,
+      `
+        While translating the Book of Mormon, Joseph Smith and his scribe Oliver Cowdery had questions about baptism under the proper authority. After going into a forest near 
+        the Susquehana River to pray about the issue, John the Baptist appeared to them in as an angel of glory, ordained them to the Aaronic priesthood, and gave them specific keys, calling them his fellow 
+        servants (D&C 13). They then baptized each other, receiving great spiritual manifestations and prophecies. Later, Peter, James, and John came to confer upon Joseph and Oliver 
+        the keys of the Apostleship, as well as the keys of the kingdom and the "dispensation of the fulness of times". These keys were necessary to lead and guide the church, and 
+        these permissions, which were taken from the earth, were restored to Joseph Smith.
+      `,
+      `
+        Today, the Church of Jesus Christ of Latter-day Saints holds the priesthood to guide the Church through modern day prophets and apostles. The priesthood blesses God's children in countless ways in day-to-day life. 
+        Through the priesthood, we have the power to overcome the world through the cleansing and sanctifying power of Jesus Christ's Atonement manifested in temple covenants and ordinances (D&C 84:20). There are five 
+        aspects of the priesthood that are important to understand:
+        <ul>
+            <li>Authority</li>
+            \tPriesthood authority is the ability to act in God's name with His approval. Authority is given upon ordination to an office of the priesthood under the presiding keys.
+            <li>Power</li>
+            \tGod's power given through obedience and righteousness (D&C 121:36-39).
+            <li>Office</li>
+            \tDivisions of priesthood authority that perform different responsibilities and ordinances. There are several offices in both the Aaronic and Melchizedek priesthoods, including deacons,
+            teachers, priests, and bishop for Aaronic and elder, high priest, seventy, apostle, and patriarch for Melchizedek. Although the office of bishop belongs to the firstborn descendents of Aaron (D&C 68:16), 
+            high priests may officiate as a bishop as well (D&C 68:14-15).
+            <li>Keys</li>
+            \tPermission from God to govern how the priesthood related to the key can be exercised i.e. the sealing key gives permission to direct sealings, gathering of Israel gives permission to 
+            direct missionary work, etc.
+            <li>Ordinances</li>
+            \tAuthorized rituals that are effectuated under thosewho hold keys. They have symbolic meaning that open the door to spiritual blessings and power.
+        </ul>
+      `,
+      `
+        Although at this time only men may hold priesthood offices, the rest of the aspects of the priesthood are accessible to women. They hold authority and keys in their callings and presidencies and they officiate
+        in ordinances in the temple. In this way, they have as much access to priesthood power as men do, and as Joseph taught, men and women can obtain the highest degree of celestial glory only by entering together 
+        into a holy order of the priesthood through the temple sealing ordinance. The priesthood unites and seals God's family, and empowers them to achieve peace in this life and eternal life in the world to come.
+        All are invited to partake of the covenants of the gospel and receive eternal life from the Father and the Son. 
+      `,
+    ],
+  },
+  covenantsAndTemples: {
+    title: 'Covenants and Temples',
+    img: '',
+    text: ['this works'],
+  },
+};
