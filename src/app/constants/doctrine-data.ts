f52f36616054a3119a834c37e64f0e58efdbbe28
nikgiuliani <anthony.giuliani26@gmail.com>
2023-04-06 17:00:52 -0600

    again

--- src/app/constants/doctrine-data.ts
+++ src/app/constants/doctrine-data.ts
@@ -1,12 +1,57 @@
 export const CORE_DOCTRINES = {
   bookOfMormon: {
     title: 'Book of Mormon',
-    img: '',
-    text: ['this works'],
+    img: 'url(../../../assets/img/gold_plates.jpeg)',
+    text: [
+      `
+        The Book of Mormon is a record of God's covenant people in America. It shows their experiences with God, their obedience and disobedience, prophets and apostasy, 
+        and even records an experience of Jesus Christ visiting the people there. After centuries of prophet historians taking care of the record, the final prophet named Moroni
+        buried the record in the earth to preserve it for our day. Centuries later, Joseph Smith, as part of his prophetic mission, was called to unearth and translate the 
+        record into English. Joseph Smith said that the Book of Mormon that it was \“the most correct of any Book on earth & the keystone of our religion & a man would get nearer to God by 
+        abiding by its precepts than by any other Book.\”
+      `,
+      `
+        The Book of Mormon was translated through a miraculous process. Joseph said that he translated it "by the gift and power of God." God manifests His power to His servants through 
+        different means. He may send angels, visions or dreams, and even the Savior Himself. At times, God has also manifested His power through physical instruments, such as Moses\' staff and 
+        Aaron's rod. Joseph Smith and his scribes wrote aboutt two instruments in the translation process. One of these was what Latter-day Saints today know of as the \"Urim 
+        and Thummim.\" These \"interpreters,\" as the Book of Mormon calls them, were spectacles found with the plates. Joseph would peer into them and he would see the translation
+        of the reformed Egyptian characters. Another instrument he used, which Joseph discovered in the ground years earlier, was an oval stone called a "seer stone." As a young man 
+        during the 1820s, Joseph Smith, like others in his day, used a seer stone to look for lost objects and buried treasure. As Joseph grew to understand his prophetic calling, 
+        he learned that he could use this stone for the higher purpose of translating the plates. 
+      `,
+      `
+        For convenience, Joseph often used the single seer stone rather than the two interpreters bound together. Because of this interchangability, he would often refer to both the seer 
+        stone and interpreters as the "Urim and Thummim." While the use of physical instruments might surprise you, it is consistent with some scriptural accounts. The actual mechanics of 
+        translation is reported by the various scribes. Some accounts say Joseph studied the physical characters on the plates and most say that he used either the seer stone or the interpreters. 
+        According to these accounts, Joseph placed either the interpreters or the seer stone in a hat, pressed his face into the hat to block out the light, and read aloud the English words 
+        that appeared on the instrument. Despite these seemingly unconventional methods of translation, all of the scribes associated with the translation unquestionably believed that Joseph
+        translated with divine power. 
+      `,
+      `
+        In conclusion, although the circumstances and methods of translation are unique, the Book of Mormon is of a divine origin, both in its translation and its contents. This is part of a
+        \"marvelous work and a wonder\" (Isa. 29:14) to commence the \"dispensation of the fulness of times\" (Eph. 1:10) and restore lost truths. Because of the Book of Mormon, we know the following 
+        truths: 
+        <ul>
+        <li>Prophets in the Americas taught pure Christianity, which is worship of Christ directly through faith, repentance, baptism, receiving the gift of the Holy Ghost, and enduring to the end by grace.</li>
+        <li>Some Native Americans descend from Israel. They are a chosen people who will fulfill the work of God in the last days, with the Gentiles assisting them</li>
+        <li>A great, revelatory latter-day work will commence in the last days, including the building of Zion or the New Jerusalem by the remnant of the seed of Joseph on the American continent, a promised land.</li>
+        <li>God speaks to all nations, and that the canon is wide open and can be added upon.</li>
+        </ul>
+
+        However, the best test for the veracity of the Book of Mormon is to read, ponder, and pray upon its truthfulness. The reader is promised to receive a testimony from the Holy Ghost 
+        upon asking for this witness; all they need to do is ask in faith.
+      `,
+      `
+        For more info on the translation of the Book of Mormon, please check out these resources:
+        <ul>
+            <li><a href="https://www.churchofjesuschrist.org/study/manual/gospel-topics-essays/book-of-mormon-translation?lang=eng" target="_blank"> Book of Mormon Translation </a></li>
+        </ul>
+      `,
+    ],
   },
   priesthood: {
     title: 'Priesthood',
-    img: '',
+    img: 'url(../../../assets/img/restoration_of_the_melchizedek_priesthood.jpeg)',
     text: [
       `
         Anciently, Jesus Christ gave His Apostles authority to \"bind on earth\" and \"in heaven\" (Matthew 16:19) and ordained them to this power (John 15:16). 
@@ -21,7 +66,12 @@ export const CORE_DOCTRINES = {
         the Susquehana River to pray about the issue, John the Baptist appeared to them in as an angel of glory, ordained them to the Aaronic priesthood, and gave them specific keys, calling them his fellow 
         servants (D&C 13). They then baptized each other, receiving great spiritual manifestations and prophecies. Later, Peter, James, and John came to confer upon Joseph and Oliver 
         the keys of the Apostleship, as well as the keys of the kingdom and the "dispensation of the fulness of times". These keys were necessary to lead and guide the church, and 
-        these permissions, which were taken from the earth, were restored to Joseph Smith.
+        these permissions, which were taken from the earth, were restored to Joseph Smith. The importance of priesthood authority and keys are best demonstrated through successsion crisis after the martyrdom of the Prophet. 
+        Although others claimed authority to lead the Church after Joseph and Hyrum's death, the Quorum of the Twelve
+        had received the priesthood authority, keys, and ordinances to continue to lead the Church in the Prophet's stead from the Prophet himself. According to an article written Ronald W. Walker about this issue, \“\'Joseph more than one score of times told . . . 
+        [the apostles] both in private and in public, that he rolled the Kingdom on to their shoulders,' [Brigham] Young would later say. Joseph\'s conferral of authority included priesthood keys of authority 
+        but also a fullness of the endowment ritual, \'everything necessary for the salvation of man.\'\" <sup><a href="https://rsc.byu.edu/firm-foundation/six-days-august-brigham-young-succession-crisis-1844" target="_blank">1</a></sup> Priesthood keys and authority, therefore, are essential in leadership of the Church. 
+        After this crisis was resolved, the keys to lead the Church continued through Brigham Young.
       `,
       `
         Today, the Church of Jesus Christ of Latter-day Saints holds the priesthood to guide the Church through modern day prophets and apostles. The priesthood blesses God's children in countless ways in day-to-day life. 
@@ -49,11 +99,68 @@ export const CORE_DOCTRINES = {
         into a holy order of the priesthood through the temple sealing ordinance. The priesthood unites and seals God's family, and empowers them to achieve peace in this life and eternal life in the world to come.
         All are invited to partake of the covenants of the gospel and receive eternal life from the Father and the Son. 
       `,
+      `
+        For more info on the history of the priesthood in the restored church, please check out these resources:
+        <ul>
+            <li><a href="https://www.churchofjesuschrist.org/study/manual/gospel-topics-essays/joseph-smiths-teachings-about-priesthood-temple-and-women?lang=eng" target="_blank"> Women and the Priesthood</a></li>
+            <li><a href="https://www.churchofjesuschrist.org/study/manual/gospel-topics-essays/race-and-the-priesthood?lang=eng" target="_blank">Race and the Priesthood</a></li>
+        </ul>
+      `,
     ],
   },
   covenantsAndTemples: {
     title: 'Covenants and Temples',
-    img: '',
-    text: ['this works'],
+    img: 'url(../../../assets/img/salt_lake_temple.jpeg)',
+    text: [
+      `
+        In ancient times, God has always commanded His people to build a house where He could come and visit His people. Moses’ people made the Tabernacle, Solomon 
+        built the temple in Jerusalem, and in the early days of the Restoration, the Lord commanded the people through the Prophet Joseph Smith to establish houses 
+        of the Lord (D&C 57:3). The temple is the place closest to God where disciples of Christ make covenants with Him. Covenants are sacred promises made between 
+        an individual and God. In the Church, individuals make covenants through baptism and continue to make covenants as they progress through various stages of 
+        the covenant path. These covenants include: 
+        <ul>
+        <li>The Law of Obedience</li>
+        \tThis covenant means to obey the Lord by striving to do whatever He commands us to do throughout the course of our lives.
+        <li>The Law of Sacrifice</li>
+        \tThis covenant means to do all we can to support the Lord’s work and repent with a broken heart and contrite spirit. In essence, we sacrifice the natural 
+          man inside each of us.
+        <li>The Law of the Gospel</li>
+        \tThis covenant means to live the higher law Jesus Christ taught while on earth, which includes faith, repentance, making and keeping sacred covenants, 
+        receiving the Gift of the Holy Ghost, and enduring to the end.
+        <li>The Law of Chastity</li>
+        \tThis covenant means that we have sexual relations only with the person to whom we are legally and lawfully wedded according to God’s law.
+        <li>The Law of Consecration</li>
+        \tThis covenant means dedicating our time, talents, and everything with which the Lord has blessed us, or everything with which He may bless us, to building 
+          up Jesus Christ’s Church on the earth.
+        </ul>
+
+      `,
+      `
+        Making and keeping covenants is a way to draw closer to God and receive His blessings. The \“endowment of power,\” where these covenants are made, should not 
+        be confused with the presentation of the endowment; the endowment of power means, according to Brigham Young, \”to receive all those ordinances in the house 
+        of the Lord, which are necessary for you, after you have departed this life, to . . . gain your eternal exaltation in spite of earth and hell.\”<sup>
+        <a href=”https://rsc.byu.edu/vol-5-no-2-2004/endowed-power” target=”_blank”>1</a></sup> This means that the power we receive through the endowment grants us 
+        the ability, through the grace and power of Jesus Christ and His Atonement, to overcome the effects of the Fall. We, like Adam and Eve, have fallen out of the 
+        presence of God; but because Jesus Christ has satisfied the demands of justice, He can offer us mercy, and empower us to become like Him through this covenantal 
+        relationship we develop with Him, that way He can “seal us his” (Mosiah 5:15).  That is why the temple is so important to the latter day work.
+      `,
+      `
+        Temples are considered to be the most sacred places of worship in the Church. They are used for performing certain ordinances and making specific covenants with 
+        God. These ordinances include baptism for the dead, sealing families together for eternity, and receiving endowments which provide additional knowledge and 
+        spiritual power. Temples are also used for proxy work, which involves performing these ordinances on behalf of deceased individuals who did not have the 
+        opportunity to make these covenants during their lifetime.
+      `,
+      `
+        The covenants made in the temple are sacred and binding for eternity. The temple is a place of peace and refuge, where individuals can go to feel closer to God 
+        and receive guidance and inspiration. Covenants and temples are essential elements of Church doctrine and practice. They are a way for individuals to connect 
+        with God, receive blessings, and progress on their spiritual journey.
+      `,
+      `
+        For more info on covenants and the holy temple, please check out these resources:
+        <ul>
+            <li><a href="https://www.churchofjesuschrist.org/temples?lang=eng" target="_blank"> Official Website on Temples </a></li>
+        </ul>
+      `,
+    ],
   },
 };
--- src/app/shared-components/doctrine-popover/doctrine-popover.component.ts
+++ src/app/shared-components/doctrine-popover/doctrine-popover.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
+import { PopoverController } from '@ionic/angular';
 
 @Component({
   selector: 'app-doctrine-popover',
   templateUrl: './doctrine-popover.component.html',
-  styleUrls: ['./doctrine-popover.component.css']
+  styleUrls: ['./doctrine-popover.component.css'],
 })
 export class DoctrinePopoverComponent {
-  doctrineData: any; 
+  doctrineData: any;
 
-  constructor() { }
+  constructor(private popoverController: PopoverController) {}
 
+  async closePage() {
+    await this.popoverController.dismiss();
+  }
 }
